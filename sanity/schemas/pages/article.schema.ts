f53f7d99b6168cc4eb9eb312cd7c59870624b7ea
niklusrook <nikolas.vardeberg1@gmail.com>
2024-12-31 16:39:10 +0100

    Create article.schema.ts

---
+++ sanity/schemas/pages/article.schema.ts
@@ -0,0 +1,114 @@
+import { blockArea } from '@/sanity/schemas/objects/blocks.schema';
+import { defineType } from 'sanity';
+import { image } from '../objects/image.schema';
+import { seo } from '../objects/seo.schema';
+import { teaserGroup } from '../objects/teaser.schema';
+
+export const article = defineType({
+	type: 'document',
+	name: 'article',
+	title: 'Article',
+	groups: [
+		{
+			name: 'general',
+			title: 'General',
+			default: true,
+		},
+		{
+			name: 'teaser',
+			title: 'Teaser',
+		},
+		{
+			name: 'byline',
+			title: 'Byline',
+		},
+		{
+			name: 'seo',
+			title: 'SEO & Routing',
+		},
+	],
+	fields: [
+		{
+			name: 'title',
+			type: 'string',
+			title: 'Tittel',
+			group: 'general',
+		},
+		{
+			name: 'entry',
+			type: 'entryRichText',
+			title: 'Ingress',
+			group: 'general',
+		},
+		{
+			name: 'publishedAt',
+			type: 'datetime',
+			title: 'Publisert',
+			group: 'general',
+			description: 'Dato for publisering av artikkelen',
+			initialValue: new Date().toISOString(),
+			validation: (Rule) => Rule.required(),
+		},
+		image({
+			name: 'mainImage',
+			group: 'general',
+			title: 'Hovedbilde',
+			description: 'Hovedbilde for artikkelen',
+		}),
+		{
+			name: 'content',
+			type: 'richText',
+			title: 'Content',
+			group: 'general',
+		},
+		blockArea({
+			group: 'general',
+		}),
+		{
+			name: 'editor',
+			title: 'Redaktør',
+			type: 'reference',
+			to: [{ type: 'editor' }],
+			group: 'byline',
+		},
+		seo({ group: 'seo' }),
+		...teaserGroup,
+	],
+	orderings: [
+		{
+			title: 'Publiseringsdato (nyeste først)',
+			name: 'publishDateDesc',
+			by: [{ field: 'publishedAt', direction: 'desc' }],
+		},
+		{
+			title: 'Publiseringsdato (eldste først)',
+			name: 'publishDateAsc',
+			by: [{ field: 'publishedAt', direction: 'asc' }],
+		},
+		{
+			title: 'Tittel (A-Å)',
+			name: 'titleAsc',
+			by: [{ field: 'title', direction: 'asc' }],
+		},
+		{
+			title: 'Tittel (Å-A)',
+			name: 'titleDesc',
+			by: [{ field: 'title', direction: 'desc' }],
+		},
+	],
+	preview: {
+		select: {
+			title: 'title',
+			publishedAt: 'publishedAt',
+			mainImage: 'mainImage',
+			teaser: 'teaserImage',
+		},
+		prepare({ title, publishedAt, mainImage, teaser }) {
+			return {
+				title,
+				subtitle: new Date(publishedAt).toLocaleDateString(),
+				media: mainImage || teaser,
+			};
+		},
+	},
+});
