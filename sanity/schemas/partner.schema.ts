c6572f1ceff50f39b9c89689fef8b03c5785dc7e
niklusrook <nikolas.vardeberg1@gmail.com>
2024-12-31 16:24:25 +0100

    Create partner.schema.ts

---
+++ sanity/schemas/partner.schema.ts
@@ -0,0 +1,39 @@
+import { defineType } from 'sanity';
+import { pages } from '../constants';
+
+export const partner = defineType({
+	name: 'partner',
+	type: 'document',
+	title: 'Partner',
+	fields: [
+		{
+			name: 'name',
+			title: 'Navn',
+			type: 'string',
+			validation: (Rule) => Rule.required(),
+		},
+		{
+			name: 'teaserText',
+			title: 'Teaser tekst',
+			type: 'internationalizedArrayText',
+			options: {
+				rows: 3,
+			},
+			description: 'Kort tekst som beskriver partneren',
+			validation: (Rule) => Rule.max(120).required(),
+		},
+		{
+			name: 'logo',
+			title: 'Logo',
+			description: 'Bilde av partnerens logo i SVG-format. (hvit)',
+			type: 'image',
+		},
+		{
+			name: 'page',
+			title: 'Side',
+			description: 'Side for partneren',
+			type: 'reference',
+            to: pages,
+		},
+	],
+});
